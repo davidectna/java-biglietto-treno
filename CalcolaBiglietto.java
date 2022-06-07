bd735c13766c5b34199c82566ae05838b5850d96
davidectna@gmail.com <INDIRIZZO_E-MAIL>
2022-06-08 00:46:37 +0200

    first commit

---
+++ CalcolaBiglietto.java
@@ -0,0 +1,15 @@
+package jana60;
+
+import java.util.Scanner;
+
+public class CalcolaBiglietto {
+	public static void main(String[] args) {
+		
+		int numeroDiChilometri;
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Inserisci età persona 1: ");
+	    numeroDiChilometri = scan.nextInt(); 
+}
+}
